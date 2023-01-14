2c7a406e32167671e18274836f07a7e54ee0454d
Ochieng Otieno <ochiengotieno71@gmail.com>
2023-01-15 01:50:52 +0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,105 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_required, current_user
+from flask_mail import Message
+from app import mail
+from .models import Alert, Student
+from . import db
+import africastalking
+import os
+import smtplib
+
+
+main = Blueprint('main', __name__)
+
+username = os.getenv('user_name', 'sandbox')
+api_key = os.getenv('api_key', 'key')
+phone = os.getenv('phone', 'phone')
+
+africastalking.initialize(username, api_key)
+sms = africastalking.SMS
+
+
+@main.route('/')
+@login_required
+def index():
+    alerts = Alert.query.all()
+    return render_template('index.html', alerts=alerts)
+
+
+@main.route('/alert')
+@login_required
+def alert():
+    return render_template('alert.html')
+
+
+@main.route('/alert', methods=['POST'])
+@login_required
+def add_alert():
+    title = request.form.get('title')
+    body = request.form.get('body')
+
+    new_alert = Alert(title=title, body=body)
+
+    db.session.add(new_alert)
+    db.session.commit()
+
+    # message = f"Dear {new_alert.name} your alert, ID: 00{new_alert.id} has been logged into our storage"
+
+    # msg = Message("alert Added",
+    #             sender=os.getenv("email"),
+    #             recipients=[new_alert.email],
+    #             body=message)
+
+    # mail.send(msg)
+
+    # sms.send(f"Dear {new_alert.name} your alert ID: 00{new_alert.id} has been logged into our storage", [f"{phone}"], callback=on_finish)
+    return redirect(url_for('main.index'))
+
+
+def on_finish(error, response):
+    if error is not None:
+        raise error
+    print(response)
+
+@main.route('/student')
+@login_required
+def student():
+    students = Student.query.all()
+    return render_template('student.html', students=students)
+
+@main.route('/student', methods=['POST'])
+@login_required
+def add_student():
+    name = request.form.get('name')
+    reg = request.form.get('reg')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    school = request.form.get('school')
+
+    new_student = Student(name=name, reg=reg, email=email, phone=phone, school=school)
+
+    db.session.add(new_student)
+    db.session.commit()
+
+    return redirect(url_for('main.student'))
+
+
+@main.route('/alert/<id>')
+@login_required
+def view_alert(id):
+    alert = Alert.query.filter_by(id=id).first_or_404()
+    data = f"Post Code: {alert.id}\n Title: {alert.title}\n Body: {alert.body}"
+    return render_template('show_alert.html', alert=alert, data=data)
+
+
+@main.route('/logout/<id>')
+@login_required
+def logout(id):
+    alert = Alert.query.filter_by(id=id).first_or_404()
+
+    db.session.delete(alert)
+    db.session.commit()
+    flash("alert logged out")
+
+    return redirect(url_for('main.index'))
+
---
+++ app/models.py
@@ -0,0 +1,30 @@
+from . import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+class Lauggage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    description = db.Column(db.Text(350))
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    reg = db.Column(db.String(30))
+    phone = db.Column(db.String(15))
+    email = db.Column(db.String(20))
+    school = db.Column(db.String(50))
+
+class Alert(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    body = db.Column(db.Text)
+
