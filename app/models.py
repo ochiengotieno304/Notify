d8bf406e37ac03918277274440205ede57e02fcb
Ochieng Otieno <ochiengotieno71@gmail.com>
2023-01-15 11:34:18 +0300

    chore(db): delete luggage table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from flask_login import UserMixin
+import datetime
+from sqlalchemy import Column, Integer, DateTime
 
 
 class User(UserMixin, db.Model):
@@ -7,13 +9,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-
-
-class Lauggage(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    description = db.Column(db.Text(350))
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,4 +22,6 @@ class Alert(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     body = db.Column(db.Text)
+    # created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
 
